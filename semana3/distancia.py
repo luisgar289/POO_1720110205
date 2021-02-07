7124422a0d0c42855a06426cb9b73e71aa6deead
Luis  Fernando <1720110205@utectulancingo.edu.mx>
2021-02-07 04:13:04 +0000

    Ejercicio completo

--- semana3/distancia.py
+++ semana3/distancia.py
@@ -9,21 +9,19 @@ class DistanciaPuntos():
 
     def __init__ (self):
         pass
-    def operacion_1(self, X1, X2, Y1, Y2):
+    def operacion(self, X1, X2, Y1, Y2):
         resultado_1 = X2-X1
         elevacion1 = resultado_1**2
         resultado_2 = Y2-Y1
         elevacion2 = resultado_2**2
         resultado3 = elevacion1+elevacion2
         final= math.sqrt(resultado3)
-        print(final)
+        print("La distancia de un punto a otro es ",final)
  
 punto = DistanciaPuntos()
-punto.X1=3.17
-punto.Y1=4.78
-punto.X2= 4.99
-punto.Y2 = 7.88
 
-punto.operacion_1(3.17,4.99,4.78,7.88)
-
-
+punto.operacion(3.17,4.99,4.78,7.88)
+punto.operacion(7.15,1.93,21.6,4.38)
+punto.operacion(12.17,10.4,10.4,29.3)
+punto.operacion(39.4,68.3,78.9,187.2)
+punto.operacion(88.7,295.3,118.3,18.4)
