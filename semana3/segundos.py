bfad97201a791768640f969e89bd33b4247c7009
Luis  Fernando <1720110205@utectulancingo.edu.mx>
2021-02-11 17:53:24 +0000

    ya casi queda

--- semana3/segundos.py
+++ semana3/segundos.py
@@ -1,12 +1,22 @@
 class Segundos():
 
     dias = 0
+    segundosDia = 0
+    final = 0
 
     def __init__ (self):
         pass
 
     def entrada (self):
         self.dias = input("Dias: ")
+    
+    def dia (self):
+        self.segundosDia = 3600 * 24
 
+    def operacion(self):
+        self.final = calculo.entrada() * calculo.dia()
+        print("Segundos: " , )
+    
 calculo = Segundos()
 calculo.entrada()
+calculo.dia()
