5b5e0a595e27a5fe3d1c2874817abaa6a6fd6ba8
Luis  Fernando <1720110205@utectulancingo.edu.mx>
2021-02-11 17:33:41 +0000

    Primer proceso

---
+++ semana3/segundos.py
@@ -0,0 +1,12 @@
+class Segundos():
+
+    dias = 0
+
+    def __init__ (self):
+        pass
+
+    def entrada (self):
+        self.dias = input("Dias: ")
+
+calculo = Segundos()
+calculo.entrada()
